c40bec06ae7f1c4dae816830fa322e8aef02f959
Iyanuoluwa <iy@gidigidiolu.com>
2020-02-20 22:09:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Instagram HashTag SlideShow
+
+This program uses Instagram Crawler (<https://github.com/huaying/instagram-crawler)> to crawl Instagram hashtags and create a slideshow (html file).
+
+This crawler may fail due to updates on instagram’s website. Please contact me if you encounter any problems.
+
+## Install
+
+1. Make sure you have Chrome browser installed.
+2. Download [chromedriver](https://sites.google.com/a/chromium.org/chromedriver/) and put it into bin folder: `./inscrawler/bin/chromedriver`
+3. Install Selenium: `pip install -r requirements.txt`
+4. `cp inscrawler/secret.py.dist inscrawler/secret.py`
+
+## Usage
+
+### Example
+
+To create a slideshow with 1000 images of #totliv. App restarts every 24 hours from first crawl unless stopped.
+
+```shell
+python hashtag.py totliv 1000
+```
+
+To create a slideshow with 1000 images of #totliv. App restarts every 360 seconds from first crawl unless stopped.
+
+```shell
+python hashtag.py totliv 1000 360
+```
+
+To explicitly supply an input html file.
+
+```shell
+python hashtag.py totliv 1000 --html slideshow.html
+
+python hashtag.py totliv 1000 360 --html sideshow.html
+```
---
+++ hashtag.py
@@ -0,0 +1,99 @@
+import datetime
+import time
+
+import click
+import progressbar
+from bs4 import BeautifulSoup
+
+from crawler import get_posts_by_hashtag
+
+
+def __create_content(img_url):
+    return (f'<div class="mySlides fade"><img src="{img_url}"></div>')
+
+
+def create_slideshow(html_file='index.html', clear_target=False,
+                     hashtags=None):
+    with open(html_file, encoding='utf8') as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+
+    target = soup.find(class_= 'slideshow-container')
+
+    if clear_target:
+        target.clear()
+
+    bar = progressbar.ProgressBar(max_value=len(hashtags),
+                                  prefix='Updating HTML file: ')
+    bar.start()
+    for hashtag in hashtags:
+        img_url = hashtag['img_url']
+        
+        # skip inserting URL if it already exists in target
+        search_result = target.find(attrs={'src': img_url})
+        if search_result is None:
+            content = __create_content(img_url)
+
+            # create a temporary document from your HTML
+            temp = BeautifulSoup(content, "html.parser")
+
+            # the nodes we want to insert are children of the <body> in `temp`
+            nodes_to_insert = temp.children
+
+            # insert them, in source order
+            for j, node in enumerate(nodes_to_insert):
+                target.insert(j, node)
+
+        bar += 1
+    bar.finish()
+    return soup
+
+
+def write_soup_to_file(output_file="index.html", soup=None):
+    with open(output_file, "w", encoding='utf-8') as file:
+        file.write(str(soup))
+
+@click.command()
+@click.argument('hashtag')
+@click.argument('number', default=100)
+@click.argument('sleep_time', default=86400)
+@click.option('--clear', is_flag=True)
+@click.option('--html', default='index.html', help='html file to update',
+              type=click.Path(exists=True))
+def main(hashtag, number, sleep_time, clear, html):
+    while True:
+        print('>> App Stated - Please be patient.. '
+              'This may take sometime to complete...')
+
+        current_time = datetime.datetime.now()
+
+        # set clear if hashtag is different
+        with open(html, encoding='utf8') as fp:
+            soup = BeautifulSoup(fp, "html.parser")
+
+        tag = soup.title.string.split(' ')[0]
+        if tag != f'#{hashtag}':
+            raise click.ClickException(f"{html} contains a different hashtag. "
+                                       "Please include the --clear flag to "
+                                       "clear images.")
+
+        hashtags = get_posts_by_hashtag(tag=hashtag, number=number)
+        if clear:
+            soup = create_slideshow(html_file=html, hashtags=hashtags,
+                                    clear_target=True)
+        else:
+            soup = create_slideshow(html_file=html, hashtags=hashtags)
+
+        # update html's title
+        soup.title.string = f'#{hashtag} SlideShow'
+        write_soup_to_file(soup=soup)
+        print(f'>> {html} has been updated.')
+
+        # sleep_time in seconds
+        next_time = current_time + datetime.timedelta(seconds=sleep_time)
+        print(f'>> App will wake in {next_time}')
+        
+        time.sleep(sleep_time)
+
+
+if __name__ == '__main__':
+    main()
